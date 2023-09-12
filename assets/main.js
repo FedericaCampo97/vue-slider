b520b6e675f515c2dd25c5bcb224dbf0c553dd1d
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-12 15:39:44 +0200

    add images whit for

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 const slides = [
-    {
-        image: 'img/01.webp',
+        {
+            image: 'img/01.webp',
             title: 'Marvel\'s Spiderman Miles Morale',
             text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
         }, {
@@ -29,14 +29,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            movie : {
-                title : 'The MandaLorian',
-                images: [
-                ],
-                category : 'action',
-                lenght : 60 ,
-
-            }
+            slides : slides
             
 
         }
