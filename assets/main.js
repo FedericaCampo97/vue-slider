235ea1a6e8263e73e9a58b99c0504ddcf47ce1a7
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-12 15:26:54 +0200

    Initial exercise

--- assets/main.js
+++ assets/main.js
@@ -1,110 +1,44 @@
-/*Modifichiamo il codice dell'esercizio per renderlo 
-funzionante con un array di oggetti al posto di un array 
-di stringhe.*/
+const slides = [
+    {
+        image: 'img/01.webp',
+            title: 'Marvel\'s Spiderman Miles Morale',
+            text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        }, {
+            image: 'img/02.webp',
+            title: 'Ratchet & Clank: Rift Apart',
+            text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        }, {
+            image: 'img/03.webp',
+            title: 'Fortnite',
+            text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        }, {
+            image: 'img/04.webp',
+            title: 'Stray',
+            text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+        }, {
+            image: 'img/05.webp',
+            title: "Marvel's Avengers",
+            text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+        }
+    ];
+    
+    console.log(slides);
 
-//Creo un array oggetto di immagini
-const photo = [
-    {
-     image: './img/01.webp'
-    },
-    {
-     image: './img/02.webp'
-    },
-    { 
-     image: './img/03.webp'
-    },
-    {
-     image: './img/04.webp'
-    },
-    {
-     image: './img/05.webp'
-    },
- ]
- 
- //Inizializzo activeSlide a 0
- let activeSlide = 0;
- 
- // Seleziono gli elementi html
- const sliderImagesEl = document.querySelector('.slider .images')
- const prevEl = document.querySelector('.prev')
- const nextEl = document.querySelector('.next')
- 
- //Faccio ciclo foreach
- photo.forEach((element,key) => {
-     const slidePath = element.image;
-     
-     //Creo l'elemtno img e verifico l'active dalla key
-     const slideMarkup = `<img class="${activeSlide === key ? 'active' : '' }" src="${slidePath}" alt="">`
-   
-     sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
- })
- 
- //Seleziono immagini create
- const slidesImages = document.querySelectorAll('.slider .images > img')
- const thumbsElement = document.querySelector('.thumbnails')
- 
- //Ciclo per creare in pagina le immagini piccole
- photo.forEach((element,key) => {
-     const thumbPath = element.image;
-     const thumbMarkup = `<img class="thumb ${activeSlide === key ? 'active' : ''}" src="${thumbPath}" alt="">`
-     //console.log(thumbMarkup);
-   
-     thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
- })
- 
- function removeActive(){
-     //Ricavo currentSlide e currentThumb
-     const currentSlide = slidesImages[activeSlide]
-     const currentThumb = document.querySelector('.thumbnails > img.active')
- 
-     // remove the active class from the current slide
-     currentSlide.classList.remove('active')
-     // remove the active class from the active thumb
-     currentThumb.classList.remove('active')
- }
- 
- function activateNextSlideAndThunmb(){
-     // select the next slide
-     const nextSlide = slidesImages[activeSlide]
-     // add the active class to the next slide
-     nextSlide.classList.add('active')
- 
-     // select the next thumb
-     const nextThumb = document.querySelectorAll('.thumb')[activeSlide]
-     // add to the next thumb the active class
-     nextThumb.classList.add('active')
- }
- 
- //Creo funzione al click del next 
- nextEl.addEventListener('click', function(){
-     removeActive()
-   
-     if (activeSlide === slidesImages.length - 1) {
-       activeSlide = 0
-     } else {
-       activeSlide++
-     }
- 
-     activateNextSlideAndThunmb()  
-   })
- 
- prevEl.addEventListener('click', function () {
-   
-     removeActive()
-   
-     if (activeSlide === 0) {
-       activeSlide = slidesImages.length - 1
-     } else {
-         activeSlide--
-     }
-     
-     activateNextSlideAndThunmb();
- })
- 
- 
- 
- 
- 
- 
- 
- 
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            movie : {
+                title : 'The MandaLorian',
+                images: [
+                ],
+                category : 'action',
+                lenght : 60 ,
+
+            }
+            
+
+        }
+    }
+}).mount('#app')
